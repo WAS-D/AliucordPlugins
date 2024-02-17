ad30b48020c0524195e0cef499b09d3edffe34fd
unknown <water.ant.blacksmith@gmail.com>
2024-02-17 16:46:24 -0500

    cnvjnbjvnb vill be ignored, and an empty message aborts the commit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,22 @@
 import com.aliucord.gradle.AliucordExtension
 import com.android.build.gradle.BaseExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
         google()
         mavenCentral()
+        // Aliucords Maven repo which contains our tools and dependencies
         maven("https://maven.aliucord.com/snapshots")
+        // Shitpack which still contains some Aliucord dependencies for now. TODO: Remove
         maven("https://jitpack.io")
     }
+
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("com.android.tools.build:gradle:7.0.4")
+        // Aliucord gradle plugin which makes everything work and builds plugins
         classpath("com.aliucord:gradle:main-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        // Kotlin support. Remove if you want to use Java
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 }
 
@@ -24,34 +28,29 @@ allprojects {
     }
 }
 
-fun Project.aliucord(
-    configuration: AliucordExtension.() -> Unit
-) = extensions.getByName<AliucordExtension>("aliucord").configuration()
+fun Project.aliucord(configuration: AliucordExtension.() -> Unit) = extensions.getByName<AliucordExtension>("aliucord").configuration()
 
-fun Project.android(
-    configuration: BaseExtension.() -> Unit
-) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "com.aliucord.gradle")
+    // Remove if using Java
     apply(plugin = "kotlin-android")
 
+    // Fill out with your info
     aliucord {
-        val baseGithubUrl = "https://raw.githubusercontent.com/WAS-D/AliucordPlugins"
-        author("phobos", 423915768191647755L)
-        updateUrl.set("$baseGithubUrl/builds/updater.json")
-        buildUrl.set("$baseGithubUrl/builds/%s.zip")
+        author("bananercream", 123456789L)
+        updateUrl.set("https://raw.githubusercontent.com/WAS-D/AliucordPlugins/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/WAS-D/REPONAME/AliucordPlugins/%s.zip")
     }
 
     android {
-        compileSdkVersion(30)
+        compileSdkVersion(31)
 
         defaultConfig {
             minSdk = 24
-            targetSdk= 30
-            versionCode = 1
-            versionName = "1.0"
+            targetSdk = 31
         }
 
         compileOptions {
@@ -59,23 +58,29 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_11
         }
 
-        tasks.withType<KotlinCompile> {
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "11" // Required
+                // Disables some unnecessary features
                 freeCompilerArgs = freeCompilerArgs +
-                    "-Xno-call-assertions" +
-                    "-Xno-param-assertions" +
-                    "-Xno-receiver-assertions"
+                        "-Xno-call-assertions" +
+                        "-Xno-param-assertions" +
+                        "-Xno-receiver-assertions"
             }
         }
     }
 
     dependencies {
-        "discord"("com.discord:discord:aliucord-SNAPSHOT")
-        "implementation"("com.aliucord:Aliucord:main-SNAPSHOT")
+        val discord by configurations
+        val implementation by configurations
 
-        "implementation"("androidx.appcompat:appcompat:1.4.0")
-        "implementation"("com.google.android.material:material:1.5.0")
+        // Stubs for all Discord classes
+        discord("com.discord:discord:aliucord-SNAPSHOT")
+        implementation("com.aliucord:Aliucord:main-SNAPSHOT")
+
+        implementation("androidx.appcompat:appcompat:1.4.0")
+        implementation("com.google.android.material:material:1.4.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.2")
     }
 }
 
